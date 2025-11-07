8146e9e5b7555a4eebb2107b83f46b44296baf4c
akthernahela <nahela00@gmail.com>
2025-11-07 12:32:15 +0100

    Create the variable for the buttons object array

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,42 @@
 export default function Main() {
+    const buttonsList = [
+        {
+            id: 1,
+            text: "HTML",
+            type: "button",
+            className: "btn btn-success"
+        },
+        {
+            id: 2,
+            text: "CSS",
+            type: "button",
+            className: "btn btn-success"
+        },
+        {
+            id: 3,
+            text: "JAVASCRIPT",
+            type: "button",
+            className: "btn btn-success"
+        },
+        {
+            id: 4,
+            text: "NODE.JS",
+            type: "button",
+            className: "btn btn-success"
+        },
+        {
+            id: 5,
+            text: "EXPRESS",
+            type: "button",
+            className: "btn btn-success"
+        },
+        {
+            id: 6,
+            text: "REACT",
+            type: "button",
+            className: "btn btn-success"
+        }
+    ];
 
     return (
         <main className="bg-dark">
@@ -7,12 +45,14 @@ export default function Main() {
                     <h1 className="card-title">Learn Web Development</h1>
                 </div>
                 <div className="card-body">
+                    {/* 
                     <button type="button" class="btn btn-success">HTML</button>
                     <button type="button" class="btn btn-success">CSS</button>
                     <button type="button" class="btn btn-success">JAVASCRIPT</button>
                     <button type="button" class="btn btn-success">NODE.JS</button>
                     <button type="button" class="btn btn-success">EXPRESS</button>
-                    <button type="button" class="btn btn-success">REACT</button>
+                    <button type="button" class="btn btn-success">REACT</button> 
+                    */}
                 </div>
             </div>
         </main>
