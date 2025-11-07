4ea458b94f87056b09b2741830ce9c86b7af362c
akthernahela <nahela00@gmail.com>
2025-11-07 12:26:21 +0100

    Add a button list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,15 +4,15 @@ export default function Main() {
         <main className="bg-dark">
             <div className="card">
                 <div className="card-header">
-                    <h1 className="card-title"></h1>
+                    <h1 className="card-title">Learn Web Development</h1>
                 </div>
                 <div className="card-body">
                     <button type="button" class="btn btn-success">HTML</button>
-                    <button type="button" class="btn btn-success">HTML</button>
-                    <button type="button" class="btn btn-success">HTML</button>
-                    <button type="button" class="btn btn-success">HTML</button>
-                    <button type="button" class="btn btn-success">HTML</button>
-                    <button type="button" class="btn btn-success">HTML</button>
+                    <button type="button" class="btn btn-success">CSS</button>
+                    <button type="button" class="btn btn-success">JAVASCRIPT</button>
+                    <button type="button" class="btn btn-success">NODE.JS</button>
+                    <button type="button" class="btn btn-success">EXPRESS</button>
+                    <button type="button" class="btn btn-success">REACT</button>
                 </div>
             </div>
         </main>
