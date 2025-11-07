287cf5edc49a0b2ed00fadeb711887683f5fa128
akthernahela <nahela00@gmail.com>
2025-11-07 12:00:08 +0100

    Add a card

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,16 @@
 export default function Main() {
 
-    return
+    return (
+        <main>
+            <div className="container">
+                <div className="card">
+                    <div className="card-header">
+                        <h1 className="card-title"></h1>
+                    </div>
+                    <div className="card-body">
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
 }
