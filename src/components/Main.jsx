7bbdd787b5e3a2cae6857a1e3f582d5a0e352c7b
akthernahela <nahela00@gmail.com>
2025-11-07 12:15:34 +0100

    Add button to the card body

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,15 +2,19 @@ export default function Main() {
 
     return (
         <main className="bg-dark">
-
             <div className="card">
                 <div className="card-header">
                     <h1 className="card-title"></h1>
                 </div>
                 <div className="card-body">
+                    <button type="button" class="btn btn-success">HTML</button>
+                    <button type="button" class="btn btn-success">HTML</button>
+                    <button type="button" class="btn btn-success">HTML</button>
+                    <button type="button" class="btn btn-success">HTML</button>
+                    <button type="button" class="btn btn-success">HTML</button>
+                    <button type="button" class="btn btn-success">HTML</button>
                 </div>
             </div>
-
         </main>
     )
 }
